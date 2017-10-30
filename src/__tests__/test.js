40be06255ce85840107c6a0c8c3ad1fbbd3aed99
Jefferson Hernandez <jeffersonh44@gmail.com>
2017-10-30 15:37:43 +0100

    add latest changes

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,15 @@
 import Reaction from '..';
+import window from 'svgdom';
+import OCL from 'openchemlib';
+
+const document = window.document;
 
 describe('main test for reaction', () => {
-    it('starting', () => {
+    it('no products on common format', () => {
+        
+    });
 
+    it('other test', () => {
+        expect(2).toBe(2);
     });
 });
